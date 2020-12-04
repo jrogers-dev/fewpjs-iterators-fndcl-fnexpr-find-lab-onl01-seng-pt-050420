e3501b080afaef0994426753d0eccf50620c8b59
James Rogers <jrogers.dev@gmail.com>
2020-12-04 20:26:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const testVar = {}
+const testVar = {};
 
 function testFunc() {
-  return "hi"
+  return "hi";
+}
+
+function superbowlWin(records) {
+  let result = records.find(
+    function(record) {
+      return record.result === "W";
+    }
+  );
+  
+  return (result ? result.year : result);
 }
